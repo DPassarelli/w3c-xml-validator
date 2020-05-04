cc6c7bb4c6d988fb66bbd4fc025a67dfb1a07e2b
David Passarelli <dpassarelli@camelotcg.com>
2020-05-03 20:43:06 -0400

    Added logic to capture warnings.

--- main.js
+++ main.js
@@ -1,16 +1,78 @@
-// const debug = require('debug')('w3c-xml-validator')
-// const FormData = require('form-data')
+const debug = require('debug')('w3c-xml-validator')
+const FormData = require('form-data')
+const htmlParser = require('node-html-parser')
 
-// const form = new FormData()
+const VALIDATION_URI = 'https://validator.w3.org/check'
 
-// form.append('fragment', '')
-// form.append('prefill', '0')
-// form.append('doctype', 'Inline')
-// form.append('prefill_doctype', 'html401')
-// form.append('group', '0')
+/**
+ * Submits the provided XML to the W3C online validation tool. Returns the
+ * provided result.
+ *
+ * @param  {String}  src   The XML to validate. It should reference a publicly-
+ *                         available DTD.
+ *
+ * @return {Promise}
+ */
+async function submitValidation (src) {
+  return new Promise((resolve, reject) => {
+    src = src || ''
 
-async function submitValidation () {
-  throw new Error()
+    if (src === '') {
+      reject(new Error('The input parameter is required and must be a non-empty string value.'))
+      return
+    }
+
+    const form = new FormData()
+    const result = {
+      isValid: true,
+      warnings: [],
+      errors: []
+    }
+
+    form.append('fragment', '')
+    form.append('prefill', '0')
+    form.append('doctype', 'Inline')
+    form.append('prefill_doctype', 'html401')
+    form.append('group', '0')
+
+    debug('Submitting form to %s...', VALIDATION_URI)
+
+    form.submit(VALIDATION_URI, (err, response) => {
+      debug('...done!')
+
+      if (err) {
+        reject(err)
+        return
+      }
+
+      const responseChunks = []
+
+      response.on('data', (chunk) => {
+        responseChunks.push(chunk.toString())
+      })
+
+      response.on('end', () => {
+        const responseBody = responseChunks.join('')
+        const htmlDocument = htmlParser.parse(responseBody)
+
+        const warningsNode = htmlDocument.querySelector('#warnings')
+
+        warningsNode.childNodes.forEach((child) => {
+          if (child.nodeType === 1) {
+            debug('Found warning: %s', child.structure)
+
+            const target = child.childNodes[0].childNodes[2]
+            debug('   third grandchild node %o', target)
+            result.warnings.push(target.rawText)
+          }
+        })
+
+        resolve(result)
+      })
+
+      response.resume()
+    })
+  })
 }
 
 module.exports = submitValidation
--- main.spec.js
+++ main.spec.js
@@ -1,5 +1,8 @@
 /* eslint-env mocha */
 
+const nock = require('nock')
+const path = require('path')
+
 /**
  * Code under test.
  * @type {any}
@@ -14,10 +17,156 @@ describe('the "w3c-xml-validator" module', function () {
     expect(actual).to.equal(expected)
   })
 
-  describe('the returned function', function () {
-    it('must be a thenable', function () {
-      const result = T()
-      return expect(result).to.be.rejected
+  describe('the value returned by the function', function () {
+    it('must be a Promise', function () {
+      const promise = T().catch(e => { /* do nothing */ })
+      expect(promise).to.have.property('then')
+    })
+  })
+
+  describe('the returned promise', function () {
+    const ERR_MESSAGE = 'The input parameter is required and must be a non-empty string value.'
+
+    it('must be rejected if the input parameter is missing', function () {
+      const promise = T()
+      return expect(promise).to.be.rejectedWith(ERR_MESSAGE)
+    })
+
+    it('must be rejected if the input parameter is `null`', function () {
+      const promise = T(null)
+      return expect(promise).to.be.rejectedWith(ERR_MESSAGE)
+    })
+
+    it('must be rejected if the input parameter is an empty string', function () {
+      const promise = T('')
+      return expect(promise).to.be.rejectedWith(ERR_MESSAGE)
+    })
+
+    describe('the fulfilled value', function () {
+      context('for a successful validation', function () {
+        let promise = null
+
+        before(function () {
+          nock('https://validator.w3.org')
+            .post('/check')
+            .replyWithFile(200, path.join(__dirname, './test/samples/success.html'))
+            .persist()
+        })
+
+        after(function () {
+          nock.cleanAll()
+        })
+
+        beforeEach(function () {
+          promise = T('<?xml version="1.0" encoding="utf-8"?>')
+        })
+
+        it('must be an object', function () {
+          return promise
+            .then(function (response) {
+              const expected = 'object'
+              const actual = typeof response
+
+              expect(actual).to.equal(expected)
+            })
+        })
+
+        it('must have a property called "isValid" with the correct value', function () {
+          return promise
+            .then(function (response) {
+              const expected = true
+              const actual = response.isValid
+
+              expect(actual).to.equal(expected)
+            })
+        })
+
+        it('must have a property called "warnings" with the correct value', function () {
+          return promise
+            .then(function (response) {
+              const expected = [
+                'No Character encoding declared at document level',
+                'Using Direct Input mode: UTF-8 character encoding assumed'
+              ]
+              const actual = response.warnings
+
+              expect(actual).to.deep.equal(expected)
+            })
+        })
+
+        it('must have a property called "errors" with the correct value', function () {
+          return promise
+            .then(function (response) {
+              const expected = []
+              const actual = response.errors
+
+              expect(actual).to.deep.equal(expected)
+            })
+        })
+      })
+
+      context('for a validation with a single error', function () {
+        let promise = null
+
+        before(function () {
+          nock('https://validator.w3.org')
+            .post('/check')
+            .replyWithFile(200, path.join(__dirname, './test/samples/single-error.html'))
+            .persist()
+        })
+
+        after(function () {
+          nock.cleanAll()
+        })
+
+        beforeEach(function () {
+          promise = T('<?xml version="1.0" encoding="utf-8"?>')
+        })
+
+        it('must be an object', function () {
+          return promise
+            .then(function (response) {
+              const expected = 'object'
+              const actual = typeof response
+
+              expect(actual).to.equal(expected)
+            })
+        })
+
+        it.skip('must have a property called "isValid" with the correct value', function () {
+          return promise
+            .then(function (response) {
+              const expected = false
+              const actual = response.isValid
+
+              expect(actual).to.equal(expected)
+            })
+        })
+
+        it('must have a property called "warnings" with the correct value', function () {
+          return promise
+            .then(function (response) {
+              const expected = [
+                'Using Direct Input mode: UTF-8 character encoding assumed'
+              ]
+              const actual = response.warnings
+
+              expect(actual).to.deep.equal(expected)
+            })
+        })
+
+        it.skip('must have a property called "errors" with the correct value', function () {
+          return promise
+            .then(function (response) {
+              const expected = [
+                'end tag for &quot;PunchOutOrderMessage&quot; omitted, but OMITTAG NO was specified'
+              ]
+              const actual = response.errors
+
+              expect(actual).to.deep.equal(expected)
+            })
+        })
+      })
     })
   })
 })
